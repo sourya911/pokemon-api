11fbd56f98e2362ce069f21aacebb27289d99d5c
sourabh <magdumsourabh37@gmail.com>
2024-06-10 20:47:49 +0530

    commit 1: created the react-app, downloaded the required liabraries like axios for fetching data and tailwind css for styling components

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App bg-black min-h-screen text-white">
+      <h1 className="text-5xl font-bold text-center">Pokémon List</h1>
+    </div>
+  );
+}
+
+export default App;
