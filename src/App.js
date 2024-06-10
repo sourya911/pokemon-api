0964837f609215c2729001e1cc92a7705e2e0244
sourabh <magdumsourabh37@gmail.com>
2024-06-10 23:25:38 +0530

    commit 4: Addressed the Error Boundraries by adding ErrorB component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './index.css';
 import PokemList from './components/PokemList';
+import ErrorB from './components/ErrorB';
 
 function App() {
   return (
-    <div className="App bg-black min-h-screen text-white">
-      <PokemList/>
-    </div>
+    <ErrorB>
+      <div className="App bg-black min-h-screen text-white">
+        <PokemList />
+      </div>
+    </ErrorB>
   );
 }
 
